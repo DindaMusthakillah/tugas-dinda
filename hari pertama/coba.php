cac7ff2bca1041908201e38cc3a58c2e92f2397c
DindaMusthakillah <musthakillahdinda@gmail.com>
2024-08-12 14:14:26 +0700

    belajar php hari 1

---
+++ hari pertama/coba.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "<h1>Bom waktu </h1>";
+print "Master piece <br>";
+?>
+
+<a href="index.php"><p>klik disini</p></a>
---
+++ hari pertama/index.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "<h1>Bom waktu </h1>";
+print "Master piece <br>";
+printf("Learning PHP is easy! <br>");
+echo "______________________________";
+?>
+
+<a href="coba.php"><p>klik disini</p></a>
