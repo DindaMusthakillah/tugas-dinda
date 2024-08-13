c26aefb2d0e3c99c3885da08e9dbda7be61eabe7
DindaMusthakillah <musthakillahdinda@gmail.com>
2024-08-13 12:11:03 +0700

    menambahkan file

---
+++ hari kedua/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$luas=7; //deklarasi dan inisialisasi variabel
+const phi=3.14;//deklarasi dan inisialisasi konstanta
+
+echo $luas; 
+echo "<br>";
+echo phi;
+echo "<br>";
+
+$luaspersegipanjang=67;
+$kelilinglingkaran=93.5;
+$nama="Dinda Musthakillah";
+$hidup=True;
+
+var_dump($luaspersegipanjang);
+var_dump($kelilinglingkaran);
+var_dump($nama);
+var_dump($hidup);
