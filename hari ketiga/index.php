d4fa344edcdf45ac2e84367a8399d5e33a67325b
DindaMusthakillah <musthakillahdinda@gmail.com>
2024-08-14 10:58:44 +0700

    menambahkan file index

---
+++ hari ketiga/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$bilangan1=10;
+$bilangan2=3;
+
+$sisaHasilBilangan=$bilangan1%$bilangan2;
+
+echo $bilangan1."modulus".$bilangan2."=".$sisaHasilBagi2,
+
+// echo $bilangan1;
+// echo "modulus";
+// echo $bilangan2;
+// echo "="
+// echo $sisaHasilBagi;
+
+//operator perbandingan
+
+$bilA=7;
+$bilB="7";
+var_dump($bilB);
+var_dump($bilA==$bilB);
